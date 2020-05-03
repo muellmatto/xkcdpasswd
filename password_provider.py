8c9a723bcd2e5e6f0cf9bf481228173378c4cf4f
matto <matto@damniam.de>
2020-05-03 20:09:56 +0200

    testing on local devices

--- password_provider.py
+++ password_provider.py
@@ -36,7 +36,7 @@ def home():
 if __name__ == "__main__":
     # app.config['TEMPLATES_AUTO_RELOAD'] = True
     # debug does autoreloading of everything
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 
 
