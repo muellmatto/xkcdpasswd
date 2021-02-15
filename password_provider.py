dc432cebca51cd7b5674df3ad40ce4c2bcc0d748
matto <matto@damniam.de>
2021-02-15 11:19:38 +0100

    added an easy_mode

--- password_provider.py
+++ password_provider.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask import jsonify
 
 from xkcdpass import xkcd_password as xp
 
+from wordlist_easy import generate_password as generate_easy_password
+
 app = Flask(__name__)
 
-def generate_password(length=4, delimiter="-"):
+def generate_password(length=4, delimiter="-", easy_mode=False):
     """returns a xkcd-style password"""
+    if easy_mode == True:
+        return generate_easy_password()
     wordfile = xp.locate_wordfile("ger-anlx")
     mywords = xp.generate_wordlist(
             wordfile=wordfile,
@@ -27,7 +32,11 @@ def generate_password(length=4, delimiter="-"):
 
 @app.route("/rest/password")
 def rest_password(length=4, delimiter="-"):
-    return jsonify(generate_password(length=length, delimiter=delimiter))\
+    if request.args.get("easy_mode") == "true":
+        easy_mode = True
+    else:
+        easy_mode = False
+    return jsonify(generate_password(length=length, delimiter=delimiter, easy_mode=easy_mode))
 
 @app.route("/")
 def home():
